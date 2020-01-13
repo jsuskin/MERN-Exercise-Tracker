0574614b2bb6ee3061aa168c054b4be0f2878611
jsuskin <jsuskin85@gmail.com>
2020-01-13 02:26:09 -0500

    first commit

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class CreateExercise extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      description: '',
+      duration: 0,
+      date: new Date(),
+      users: []
+    }
+  }
+
+  onChangeUsername(e) {
+    this.setState({ username: e.target.value });
+  }
+
+  onChangeDescription(e) {
+    this.setState({ description: e.target.value });
+  }
+
+  onChangeDuration(e) {
+    this.setState({ duration: e.target.value });
+  }
+
+  onChangeDate(date) {
+    this.setState({ date: date })
+  }
+
+  render() {
+    return (
+      <div>CreateExercise</div>
+    );
+  }
+
+}
+
+export default CreateExercise;
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ExercisesList extends Component {
+
+  render() {
+    return (
+      <div>ExercisesList</div>
+    );
+  }
+
+}
+
+export default ExercisesList;
